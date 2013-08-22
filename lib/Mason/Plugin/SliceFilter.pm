752dfb5cb3693701cb9d979c95d91fa46f8a0807
Jerome Eteve <jerome@eteve.net>
2013-08-22 23:23:33 +0100

    Module created

---
+++ lib/Mason/Plugin/SliceFilter.pm
@@ -0,0 +1,111 @@
+package Mason::Plugin::SliceFilter;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Mason::Plugin::SliceFilter - The great new Mason::Plugin::SliceFilter!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Mason::Plugin::SliceFilter;
+
+    my $foo = Mason::Plugin::SliceFilter->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Jerome Eteve, C<< <jerome.eteve at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-mason-plugin-slicefilter at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Mason-Plugin-SliceFilter>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Mason::Plugin::SliceFilter
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Mason-Plugin-SliceFilter>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Mason-Plugin-SliceFilter>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Mason-Plugin-SliceFilter>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Mason-Plugin-SliceFilter/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Jerome Eteve.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Mason::Plugin::SliceFilter
